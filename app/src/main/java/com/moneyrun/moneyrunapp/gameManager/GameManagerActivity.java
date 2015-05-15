ffbb06f8aaba12bc9f542494c09753e59ab58af9
chvis002 <chvis002@gmail.com>
2015-05-15 22:34:45 +0200

    added stupid comment in Android studio

--- app/src/main/java/com/moneyrun/moneyrunapp/gameManager/GameManagerActivity.java
+++ app/src/main/java/com/moneyrun/moneyrunapp/gameManager/GameManagerActivity.java
@@ -11,7 +11,7 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
-
+// added stupid comment
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
